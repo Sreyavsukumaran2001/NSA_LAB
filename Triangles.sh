89a9c6bb5b8e105b139f109436e0e38440d843d4
Sreyavsukumaran2001 <106651071+Sreyavsukumaran2001@users.noreply.github.com>
2023-06-12 15:47:30 +0530

    Update Triangles.sh

--- Triangles.sh
+++ Triangles.sh
@@ -19,3 +19,23 @@ exit
 else
 echo "$a $b $c can be the sides of triangle"
 fi
+
+output
+----------
+ksb@ksb-Lenovo-Product:~/Desktop/sreya/programs$ sh Triangles.sh 
+enter first number
+1
+enter second number
+2
+enter third number
+3
+1 2 3 cannot be the sides of triangle
+ksb@ksb-Lenovo-Product:~/Desktop/sreya/programs$ sh Triangles.sh 
+enter first number
+3
+enter second number
+4
+enter third number
+5
+3 4 5 can be the sides of triangle
+
